bc7a4ea46642bb1fea11b793b38f4146cc3ce7c2
lectricas <lectricas@gmail.com>
2022-06-11 00:16:02 +0300

    feature_lexer

---
+++ src/main/java/org/intellij/sdk/language/SimpleLanguage.kt
@@ -0,0 +1,18 @@
+package org.intellij.sdk.language
+
+import com.intellij.lang.Language
+import com.intellij.openapi.fileTypes.LanguageFileType
+import com.intellij.openapi.vfs.VirtualFile
+import java.nio.charset.StandardCharsets
+
+object SimpleLanguage1 : Language("Simple")
+
+
+object SimpleFileType1 : LanguageFileType(SimpleLanguage1) {
+    override fun getDisplayName() = "Simple"
+    override fun getName() = "Simple"
+    override fun getDescription() = "Simple"
+    override fun getDefaultExtension() = "simple"
+    override fun getIcon() = SimpleIcons.FILE
+    override fun getCharset(file: VirtualFile, content: ByteArray): String = StandardCharsets.UTF_8.name()
+}
